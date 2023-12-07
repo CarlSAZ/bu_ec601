c8ed1920927b28b3eeb9e5f939acc3cc55f33efe
Carl S <carl4ece@bu.edu>
2023-12-07 18:12:43 -0500

    Making updates to the airship_teleop_keyboard

--- airship/scripts/airship_teleop_keyboard.py
+++ airship/scripts/airship_teleop_keyboard.py
@@ -5,15 +5,14 @@
 from __future__ import print_function
 
 import threading
+import copy
+import sys
 
 #import roslib
-import rospy
-
-from airship.msg import Rotor
-from airship.msg import RotorPair
-
-import sys
 from select import select
+import rospy
+from airship.msg import RotorPair, Rotor, AirshipParams
+
 
 if sys.platform == 'win32':
     import msvcrt
@@ -39,6 +38,9 @@ For Holonomic mode (strafing), hold down the shift key:
 t : up (+z)
 b : down (-z)
 
+g : 
+a : 
+
 anything else : stop
 
 q/z : increase/decrease max speeds by 10%
@@ -51,9 +53,11 @@ BASE_PWM = 20
 BASE_VERT_PWM = 20
 INVERT_LEFT = False
 INVERT_RIGHT = False
+INVERT_VERT = False
 RATE_HZ = 10 # Hz
 
 moveBindings = {
+        'k':(0,0,0,0),
         'i':(1,0,0,0),
         'o':(1,0,0,-1),
         'j':(0,0,0,1),
@@ -62,25 +66,34 @@ moveBindings = {
         ',':(-1,0,0,0),
         '.':(-1,0,0,1),
         'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
+        'K':(0,0,0,0),
+        'O':(1,0,0,-1),
         'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
+        'J':(0,0,0,1),
+        'L':(0,0,0,-1),
+        'U':(1,0,0,1),
         '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
+        '>':(-1,0,0,1),
+        'M':(-1,0,0,-1),
+    }
+VerticalBindings = {
         't':(0,0,1,0),
         'b':(0,0,-1,0),
-    }
+        'T':(0,0,1,0),
+        'B':(0,0,-1,0)
+}
 
-VerticalBindings={
-        'a':(0),
-        's':(-10),
-        'd':(-1),
-        'f':(1),
-        'g':(10),
-    }
+
+AltitudeBindings={
+        'a':(-.10), # increment set height by -10cm
+        's':(-.01), # increment by 1cm
+        'd':(.01),  # increment by 1cm
+        'f':(.10), # increment by 10cm
+        'A':(-10), # increment set height by -1m
+        'S':(-.10), # increment by 10cm
+        'D':(.10),  # increment by 10cm
+        'F':(1.0), # increment by 1m
+}
 speedBindings={
         'q':(1.1,1.1),
         'z':(.9,.9),
@@ -95,12 +108,15 @@ class PublishThread(threading.Thread):
         super(PublishThread, self).__init__()
         self.yaw_publisher = rospy.Publisher('yaw_rotors', RotorPair, queue_size = 1)
         self.alt_publisher = rospy.Publisher('altitude_rotor', Rotor, queue_size = 1)
+        self.pilot_publisher = rospy.Publisher('/airship/pilot_params', AirshipParams, queue_size = 1)
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
         self.th = 0.0
         self.speed = 0.0
         self.turn = 0.0
+        self.z_target = 1
+        self.enable_alt_control = False
         self.condition = threading.Condition()
         self.done = False
 
@@ -124,14 +140,22 @@ class PublishThread(threading.Thread):
         if rospy.is_shutdown():
             raise Exception("Got shutdown request before subscribers connected")
 
-    def update(self, x, y, z, th, speed, turn):
+    def update(self, newx, newy, newz, newth, newspeed, newturn):
         self.condition.acquire()
-        self.x = x
-        self.y = y
-        self.z = z
-        self.th = th
-        self.speed = speed
-        self.turn = turn
+        self.x = newx
+        self.y = newy
+        self.z = newz
+        self.th = newth
+        self.speed = newspeed
+        self.turn = newturn
+        # Notify publish thread that we have a new message.
+        self.condition.notify()
+        self.condition.release()
+
+    def updateAlt(self,new_ztarget,new_enable):
+        self.condition.acquire()
+        self.z_target = new_ztarget
+        self.enable_alt_control = new_enable
         # Notify publish thread that we have a new message.
         self.condition.notify()
         self.condition.release()
@@ -147,15 +171,26 @@ class PublishThread(threading.Thread):
         left_speed = self.speed*self.x + self.turn*self.th
         right_speed = self.speed*self.x - self.turn*self.th
         rotor_msg.left.pwm = int(abs(left_speed))
-        rotor_msg.left.direction = left_speed > 0
+        rotor_msg.left.direction = bool(left_speed > 0 != INVERT_LEFT)
         rotor_msg.right.pwm = int(abs(right_speed))
-        rotor_msg.right.direction = right_speed > 0
+        rotor_msg.right.direction = bool(right_speed > 0 != INVERT_RIGHT)
         return rotor_msg
+
+    def convertToZMotor(self):
+        rotor_msg = Rotor()
+        z_speed = self.speed*self.z
+        rotor_msg.pwm = int(abs(z_speed))
+        rotor_msg.direction = bool(z_speed > 0 != INVERT_VERT)
+        return rotor_msg
+
 
 
     def run(self):
         rotor_msg = RotorPair()
         last_rotor_msg = RotorPair()
+        Zrotor_msg = Rotor()
+        pilot_msg = AirshipParams()
+        last_pilot_msg = AirshipParams()
 
         while not self.done:
             self.condition.acquire()
@@ -165,18 +200,33 @@ class PublishThread(threading.Thread):
             # Copy state into twist message.
             rotor_msg = self.convertToYaw()
 
+            Zrotor_msg = self.convertToZMotor()
+            pilot_msg.header.stamp = rospy.Time.now()
+            pilot_msg.height_target_m = float(self.z_target)
+            pilot_msg.altitude_control_flag = bool(self.enable_alt_control)
             self.condition.release()
+
+
+            if last_pilot_msg != pilot_msg:
+                last_pilot_msg = copy.deepcopy(pilot_msg)
+                rospy.loginfo(f"Set new altitude control: enabled = {pilot_msg.altitude_control_flag}, height = {pilot_msg.height_target_m :.3g}m")
+            self.pilot_publisher.publish(pilot_msg)
 
             # Publish.
             if last_rotor_msg != rotor_msg:
-                self.yaw_publisher.publish(rotor_msg)
+                last_rotor_msg = copy.deepcopy(rotor_msg)
                 rospy.loginfo("\nSet rotor pair pwms: %s, %s, directions: %s, %s", rotor_msg.left.pwm, rotor_msg.right.pwm, rotor_msg.left.direction, rotor_msg.right.direction)
+            self.yaw_publisher.publish(rotor_msg)
 
+            self.alt_publisher.publish(Zrotor_msg)
         # Publish stop message when thread exits.
         rotor_msg.left.pwm = 0
         rotor_msg.right.pwm = 0
+        Zrotor_msg.pwm = 0
+        Zrotor_msg.direction = 0
 
         self.yaw_publisher.publish(rotor_msg)
+        self.alt_publisher.publish(Zrotor_msg)
 
 
 def getKey(settings, timeout):
@@ -230,6 +280,9 @@ if __name__=="__main__":
     th = 0
     status = 0
 
+    ztarget = 0
+    enable = False
+
     try:
         pub_thread.wait_for_subscribers()
         pub_thread.update(x, y, z, th, speed, turn)
@@ -238,12 +291,18 @@ if __name__=="__main__":
         print(vels(speed,turn))
         while(1):
             key = getKey(settings, key_timeout)
-            if key in moveBindings.keys():
+            if key in moveBindings:
                 x = moveBindings[key][0]
                 y = moveBindings[key][1]
-                z = moveBindings[key][2]
+                #z = moveBindings[key][2]
                 th = moveBindings[key][3]
-            elif key in speedBindings.keys():
+            elif key in VerticalBindings:
+                z = moveBindings[key][2]
+            elif key in AltitudeBindings:
+                ztarget = ztarget + AltitudeBindings[key]
+            elif key in ['g','G']:
+                enable = not enable
+            elif key in speedBindings:
                 speed = min(speed_limit, speed * speedBindings[key][0])
                 turn = min(turn_limit, turn * speedBindings[key][1])
                 if speed == speed_limit:
@@ -267,6 +326,7 @@ if __name__=="__main__":
                     break
 
             pub_thread.update(x, y, z, th, speed, turn)
+            pub_thread.updateAlt(ztarget,enable)
 
     except Exception as e:
         print(e)
