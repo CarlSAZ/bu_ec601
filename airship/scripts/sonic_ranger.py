17461e78737bd96227cfd7c5a41ea54c96605920
Carl S <carl4ece@bu.edu>
2023-12-07 00:11:04 -0500

    Changing to gpiozero library for sonic sensor

--- airship/scripts/sonic_ranger.py
+++ airship/scripts/sonic_ranger.py
@@ -2,7 +2,7 @@
 
 import rospy
 import time
-import RPi.GPIO as GPIO
+from gpiozero import DistanceSensor
 from airship.msg import Range as RangeMsg
 
 ECHO = 23
@@ -10,35 +10,21 @@ TRIG = 24
 
 RATE = 10 # Hz
 
-TIMEOUT = 1
-
-SPEED_OF_SOUND_IN_AIR_DIV2 = 171.50
 
 class SonicRanger:
     def __init__(self):
-        GPIO.setup(TRIG,GPIO.OUT)
-        GPIO.setup(ECHO,GPIO.IN)
-
-        GPIO.output(TRIG,False)
         self.pub = rospy.Publisher('airship/alt_range', RangeMsg, queue_size=10)
         rospy.init_node('SonicRanger')
+        self.sensor = DistanceSensor(echo=ECHO,trigger=TRIG,max_distance=5)
 
     def measure_range_m(self) -> float:
-        time_call = time.time()
-        GPIO.output(TRIG,True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG,False)
-
-        while GPIO.input(ECHO) ==0:
-            pulse_start = time.time()
-            if pulse_start - time_call > TIMEOUT:
-                return -1
-        while GPIO.input(ECHO) ==1:
-            pulse_end = time.time()
-            if pulse_end - time_call > TIMEOUT:
-                return -1
-        pulse_duration = pulse_end - pulse_start
-        return (pulse_duration*SPEED_OF_SOUND_IN_AIR_DIV2)
+        rangelist = []
+        for x in range(10):
+            rangelist.append(self.sensor.distnace)
+        subranges = list(dict.fromkeys(rangelist))
+        if len(subranges) < 8:
+            return -1
+        return sum(rangelist) / len(rangelist)
 
 
     def run(self):
@@ -49,12 +35,15 @@ class SonicRanger:
             msg.header.stamp = rospy.Time.now()
             msg.header.seq=count
             msg.range = self.measure_range_m()
+            if msg.range == -1:
+                print("Got bad range value!")
+                rate.sleep()
+                continue
+
             self.pub.publish(msg)
             print("Got Range of ",round(msg.range,4),"m")
             rate.sleep()
-    
-    def __del__(self):
-        GPIO.cleanup([ECHO,TRIG])
+
 
 if __name__ == '__main__':
     ranger = SonicRanger()
